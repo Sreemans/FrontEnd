e662f3072c1a730e67407d99173ae08016dc95cb
Sreeman <sreeman.eb@gmail.com>
2020-08-30 11:13:59 +0530

    Sample Html Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+const Tags = ['<html></html>', '<div></div>', '<a></a>', '<ul></ul>', '<li></li>']
 function App() {
   return (
     <div className="App">
-     {"<html></html>"}
-     {"<html></html>"}{"<html></html>"}{"<html></html>"}{"<html></html>"}{"<html></html>"}{"<html></html>"}{"<html></html>"}{"<html></html>"}
+      <div className='HtmlTags'>
+          {Tags.map(d => { return <li>{d}</li> })}
+      </div>
     </div>
   );
 }
