b15f9d525a39ac21728a872b76acf075811fae8f
Sreeman <sreeman.eb@gmail.com>
2020-08-30 10:32:19 +0530

    Updates

---
+++ precache-manifest.fb048a5a875c70cd05bcd1701b361330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8556dd60b544f29189a8294dcdf0cabc",
+    "url": "/FrontEnd/index.html"
+  },
+  {
+    "revision": "886ba82bafe4493147ca",
+    "url": "/FrontEnd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "199f4f17d6e8b4e5c2b3",
+    "url": "/FrontEnd/static/js/2.1345eff5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FrontEnd/static/js/2.1345eff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886ba82bafe4493147ca",
+    "url": "/FrontEnd/static/js/main.73d47aef.chunk.js"
+  },
+  {
+    "revision": "e8506b6cc65bba632be7",
+    "url": "/FrontEnd/static/js/runtime-main.ae3667ad.js"
+  }
+]);
