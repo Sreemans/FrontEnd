84f7ec352a0790436d127d1c3ae95377fa4a4498
Sreeman <sreeman.eb@gmail.com>
2020-10-03 12:12:11 +0530

    TICTACTOE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sample from './'
-import './App.css';
+import './App.scss';
 
 
 const Tags = ['<html></html>', '<div></div>', '<a></a>', '<ul></ul>', '<li></li>']
@@ -10,19 +10,31 @@ export default class App extends React.Component {
   }
 
   render() {
-    if (window.innerWidth == 320) {
-      return (
-        <h1 style={{ textAlign: "center" }}>Sai Teja</h1>
-      )
-    } else {
-      return (
-        <div className="App">
-         <h1> {window.innerWidth}</h1>
-          {/* <div className='HtmlTags'>
-            {Tags.map(d => { return <li>{d}</li> })}
-          </div> */}
+    let a = (window.innerWidth / 3) - 8 + 'px';
+    let b = <div id="ChildBox" style={{ width: a, height: a }}></div>;
+    let c = <div id="Coins" style={{ width: '30px', height: '30px', borderRadius: '25px', background: 'grey' }}></div>;
+    let d = <div id="Coins" style={{ width: '30px', height: '30px', borderRadius: '25px', background: 'red' }}></div>;
+    return (
+      <div className="App">
+        <div id="Box" style={{ width: window.innerWidth - '10px', height: window.innerWidth }}>
+          {b}{b}{b}{b}{b}{b}{b}{b}{b}
         </div>
-      );
-    }
+        <div id="Player">
+          <div style={{ width: '50%' }}>
+            <label>
+              Player 1
+            </label>
+             <div id='P1'>{c}{c}{c}</div>
+          </div>
+          <div style={{ width: '50%' }}>
+            <label>
+              Player 2
+            </label>
+            <div id='P2'>{d}{d}{d}</div>
+          </div>
+        </div>
+      </div>
+    );
+    // }
   }
 }
